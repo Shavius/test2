3d570187b635ecf2814cbc49d49e2f214693c527
Shavius <glamurparty@ukr.net>
2021-01-05 00:35:19 +0200

    Update 2

--- src/js/script.js
+++ src/js/script.js
@@ -32,6 +32,6 @@ let trianna = {
     'age': 30,
 };
 
-let fnTriannaInfoLog = person.logInfo.bind(trianna);
+let fnTriannaInfoLog = person.logInfo.bind(trianna,'Frontend', 1000);
 
-fnTriannaInfoLog('Frontend', 1000);
+fnTriannaInfoLog();
